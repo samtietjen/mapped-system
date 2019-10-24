98382da4adb6f436dc775a2b31688dd8e38829d7
Sam Tietjen <samtietjen@gmail.com>
2019-10-24 13:38:48 -0400

    Remove style injection

--- packages/mapped-components/src/index.js
+++ packages/mapped-components/src/index.js
@@ -1,22 +1,60 @@
 import React from 'react';
 import useMappedClasses from 'mapped-classes';
-import mapped from './mapped';
-import emotion from './emotion';
-import { mergeObjFunc } from './utilities';
+
+export const joinStrings = (...args) => {
+  const next = args.filter(v => v);
+  return next.length ? next.join(" ") : null;
+};
+
+export const mergeObjFunc = (obj, func) =>
+  typeof func === "function" 
+    ? { ...obj, ...func(obj) } 
+    : obj;
+
+export const omit = (obj, keys) => {
+  const next = {};
+  for (let key in obj) {
+    if (keys.indexOf(key) > -1) continue;
+    next[key] = obj[key];
+  }
+  return next;
+};
+
+const getAttributes = (props, mapper) => {
+  const { 
+    base = '', 
+    blacklist = [], 
+    className = '', 
+    component,
+    ...otherProps 
+  } = props;
+
+  // Omit blacklisted, utility, and mapping props.
+  const mappingKeys = Object.keys(mapper.mappings);
+  const omissions = [...blacklist, ...mappingKeys, 'tag'];
+  const attributes = omit(otherProps, omissions);
+  
+  // Join the base, mapped, and element class names.
+  const mappedClassString = mapper(otherProps);
+  attributes.className = joinStrings(base, mappedClassString, className);
+
+  return attributes;
+}
 
 const createUseMapper = config => (mappings, callback) => {
   const { breakpoints } = config;
 
-  const mapper = useMappedClasses({ ...config, mappings, breakpoints, output: 'object' });
+  const mapper = useMappedClasses({ ...config, mappings, breakpoints });
 
   const MappedComponent = React.forwardRef((props, ref) => {
-    const { cssProps } = MappedComponent;
+    const { tag = 'div' } = props;
     const propsWithCallback = { ...mergeObjFunc(props, callback), ref }
-    const constructor = cssProps || props.mx ? emotion : mapped;
-    return constructor(propsWithCallback, mapper, breakpoints, cssProps);
+    const attributes = getAttributes(propsWithCallback, mapper);
+    return React.createElement(tag, attributes);;
   });
 
   MappedComponent.isMappedComponent = true;
+  MappedComponent.mapper = mapper;
   MappedComponent.mappings = mappings;
   MappedComponent.breakpoints = breakpoints;
 
---
+++ packages/mapped-components/tests/index.test.js
@@ -0,0 +1,102 @@
+import React from "react";
+import PropTypes from 'prop-types';
+import renderer from 'react-test-renderer';
+import { matchers } from 'jest-emotion';
+import { render, cleanup } from '@testing-library/react';
+import createUseMapper from '../src';
+
+const useMapper = createUseMapper({
+  breakpoints: [
+    { label: null, minWidth: 0 }, 
+    { label: "md", minWidth: '600px' },
+    { label: "lg", minWidth: '1000px' }
+  ],
+  getter: ({ breakpoint, root, value }) =>
+    [breakpoint, root, value]
+      .filter(x => x && value !== false || x === 0)
+      .join('-')
+});
+
+export const Box = useMapper({
+  size: 'box-size'
+});
+
+Box.propTypes = {
+  base: PropTypes.string,
+  blacklist: PropTypes.array,
+  tag: PropTypes.string,
+  size: PropTypes.any
+}
+
+afterEach(cleanup);
+
+expect.extend(matchers);
+
+const createJson = Comp => renderer.create(Comp).toJSON();
+
+test('Create a component.', () => {
+  const json = createJson(<Box size={1}>Hello, World!</Box>);
+  expect(json.children).toEqual(['Hello, World!']);
+  expect(json.props.className).toEqual('box-size-1');
+  expect(json).toMatchSnapshot();
+});
+
+test('Accept element attributes.', () => {
+  const json = createJson(<Box id="my-id" size={1} />);
+  expect(json.props.id).toEqual('my-id');
+  expect(json.props.className).toEqual('box-size-1');
+  expect(json).toMatchSnapshot();
+});
+
+test('Accepts classNames.', () => {
+  const a = createJson(<Box className="my-class" />);
+  const b = createJson(<Box className="my-class" size={1} />);
+  expect(a.props.className).toEqual('my-class');
+  expect(b.props.className).toEqual('box-size-1 my-class');
+  expect(a).toMatchSnapshot();
+  expect(b).toMatchSnapshot();
+});
+
+test('Reject null values.', () => {
+  const json = createJson(<Box size={null} />);
+  expect(json).toMatchSnapshot();
+});
+
+test('Add responsive classes when passing an array.', () => {
+  const json = createJson(<Box size={[1, 2, 3]} />);
+  expect(json.props.className).toEqual('box-size-1 md-box-size-2 lg-box-size-3');
+  expect(json).toMatchSnapshot();
+});
+
+test('Prepend a class to the class list', () => {
+  const json = createJson(<Box size={1} base="box" />);
+  expect(json.props.className).toEqual('box box-size-1');
+  expect(json).toMatchSnapshot();
+});
+
+test('Transform the HTML tag', () => {
+  const json = createJson(<Box tag="h2" />);
+  expect(json.type).toEqual('h2');
+  expect(json).toMatchSnapshot();
+});
+
+test('Block attributes from an element.', () => {
+  const json = createJson(<Box href="#" blacklist={['href']} />);
+  expect(json.props.href).toBeUndefined(); 
+  expect(json).toMatchSnapshot();
+});
+
+test('Accepts functions as arguments.', () => {
+  const append = (a, b) => [a, b].filter(Boolean).join(' ');
+  const hasSize = ({ size, className }) => ({ className: append(className, size && 'has-size') });
+  const Comp = useMapper({size: 'box-size'}, hasSize);
+  const json = createJson(<Comp size={1} />);
+  expect(json).toMatchSnapshot();
+  expect(json.props.className).toEqual('box-size-1 has-size');  
+});
+
+test('Forwards ref.', () => {
+  const ref = React.createRef(null);
+  const tree = render(<Box ref={ref} />);
+  expect(ref.current.tagName).toBe('DIV')
+});
